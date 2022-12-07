9d69fe78bcba52e3c656e416660fd9a06875f2fd
Richard <richard@getcerebral.com>
2022-12-06 20:25:36 -0600

    finish intro exercises

--- exercises/intro/intro2.rs
+++ exercises/intro/intro2.rs
@@ -2,8 +2,8 @@
 // Make the code print a greeting to the world.
 // Execute `rustlings hint intro2` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
 
 fn main() {
-    println!("Hello {}!");
+    const name: &str = "Richard";
+    println!("Hello {name}!");
 }
